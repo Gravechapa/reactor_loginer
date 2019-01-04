a092b9e15a4966494672faf6ea20f717be254bf7
Gravechapa <Gravechapa@protonmail.com>
2019-01-05 02:20:12 +0300

    Minor update

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include <sys/wait.h>
 #include <unistd.h>
 #include <signal.h>
+#include <thread>
 
 static pid_t pid = 0;
 
@@ -20,13 +21,14 @@ int main()
         });
     while ((pid = fork()) > 0)
     {
-        std::cout << "Deamon started" << std::endl;
+        std::cout << "Daemon started" << std::endl;
         wait(NULL);
+        std::this_thread::sleep_for(std::chrono::seconds(5));
     }
     switch(pid)
     {
       case -1:
-          std::cerr << "Fork fail. Deamon stoped" << std::endl;
+          std::cerr << "Fork fail. Daemon stoped" << std::endl;
           exit(1);
       case 0:
           try
